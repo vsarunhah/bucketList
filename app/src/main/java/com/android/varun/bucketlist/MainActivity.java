326d73b797c0ba223930cafe2e5c01ba86235e46
Varun Shah <varunshah660@gmail.com>
2016-07-06 16:51:44 +0530

    add node

--- app/src/main/java/com/android/varun/bucketlist/MainActivity.java
+++ app/src/main/java/com/android/varun/bucketlist/MainActivity.java
@@ -2,12 +2,34 @@ package com.android.varun.bucketlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    EditText editText;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.nodeEntry);
+        submit = (Button) findViewById(R.id.enterNode);
+
+        database = FirebaseDatabase.getInstance();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference categoryRef = database.getReference(constants.FIREBASE_LOCATION_CATEGORY);
+                categoryRef.push().setValue(editText.getText().toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/android/varun/bucketlist/constants.java
+++ app/src/main/java/com/android/varun/bucketlist/constants.java
@@ -2,5 +2,6 @@ package com.android.varun.bucketlist;
 
 public class constants
 {
-    
+    public static final String FIREBASE_BASE_URL = "https://things-to-do-in-life.firebaseio.com/";
+    public static final String FIREBASE_LOCATION_CATEGORY = "bucketListCategory";
 }
